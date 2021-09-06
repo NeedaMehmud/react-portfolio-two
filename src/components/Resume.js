d8f72ef1623fe979a95eb3f923114ccb23f156d1
Needa Mehmud <needa.mehmud@gmail.com>
2021-09-06 00:56:46 -0400

    added download link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,136 +1,141 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Pdf from "./NeedaMehmud-Resume.pdf";
 
 const useStyles = makeStyles((theme) => ({
-    mainContainer: {
-        background: "#233",
+  mainContainer: {
+    background: "#233",
+  },
+  timeLine: {
+    position: "relative",
+    padding: "1rem",
+    margin: "o auto",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      height: "100%",
+      border: "1px solid tan",
+      right: "40px",
+      top: 0,
     },
-    timeLine: {
-        position: "relative",
-        padding: "1rem",
-        margin: "o auto",
-        "&:before": {
-            content: "''",
-            position: "absolute",
-            height: "100%",
-            border: "1px solid tan",
-            right: "40px",
-            top: 0,
-        },
-        "&:after": {
-            content: "''",
-            display: "table",
-            clear: "both",
-        },
-        [theme.breakpoints.up("md")]: {
-            padding: "2rem",
-            "&:before": {
-                left: "calc(50% - 1px)",
-                right: "auto",
-            },
-        },
+    "&:after": {
+      content: "''",
+      display: "table",
+      clear: "both",
     },
-    timeLineItem: {
-        padding: "1rem",
-        borderBottom: "2px solid tan",
-        position: "relative",
-        margin: "1rem 3rem 1rem 1rem",
-        clear: "both",
-        "&:after": {
-            content: "''",
-            position: "absolute",
-        },
-        "&:before": {
-            content: "''",
-            position: "absolute",
-            right: "-0.625rem",
-            top: "calc(50% - 5px)",
-            borderStyle: "solid",
-            borderColor: "tomato tomato transparent transparent",
-            borderWidth: "0.625rem",
-            transform: "rotate(45deg)",
-        },
-        [theme.breakpoints.up("md")]: {
-            width: "44%",
-            margin: "1rem",
-            "&:nth-of-type(2n)": {
-                float: "right",
-                margin: "1rem",
-                borderColor: "tan",
-            },
-            "&:nth-of-type(2n):before": {
-                right: "auto",
-                left: "-0.625rem",
-                borderColor: "transparent transparent tomato tomato",
-            },
-        },
+    [theme.breakpoints.up("md")]: {
+      padding: "2rem",
+      "&:before": {
+        left: "calc(50% - 1px)",
+        right: "auto",
+      },
     },
-    timeLineYear: {
-        textAlign: "center",
-        maxWidth: "9.375rem",
-        margin: "0 3rem 0 auto",
-        fontSize: "1.8rem",
-        color: "#fff",
-        background: "tomato",
-        lineHeight: 1,
-        padding: "0.5rem 1rem",
-        "&:before": {
-            display: "none",
-        },
-        [theme.breakpoints.up("md")]: {
-            textAlign: "center",
-            margin: "0 auto",
-            "&:nth-of-type(2n)": {
-                float: "none",
-                margin: "0 auto",
-            },
-            "&:nth-of-type(2n):before": {
-                display: "none",
-            },
-        },
+  },
+  timeLineItem: {
+    padding: "1rem",
+    borderBottom: "2px solid tan",
+    position: "relative",
+    margin: "1rem 3rem 1rem 1rem",
+    clear: "both",
+    "&:after": {
+      content: "''",
+      position: "absolute",
     },
-    heading: {
-        color: "tomato",
-        padding: "3rem 0",
-        textTransform: "uppercase",
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      right: "-0.625rem",
+      top: "calc(50% - 5px)",
+      borderStyle: "solid",
+      borderColor: "tomato tomato transparent transparent",
+      borderWidth: "0.625rem",
+      transform: "rotate(45deg)",
     },
-    subHeading: {
-        color: "#fff",
-        padding: 0,
-        textTransform: "uppercase",
+    [theme.breakpoints.up("md")]: {
+      width: "44%",
+      margin: "1rem",
+      "&:nth-of-type(2n)": {
+        float: "right",
+        margin: "1rem",
+        borderColor: "tan",
+      },
+      "&:nth-of-type(2n):before": {
+        right: "auto",
+        left: "-0.625rem",
+        borderColor: "transparent transparent tomato tomato",
+      },
     },
-    body1: {
-        color: "tomato",
+  },
+  timeLineYear: {
+    textAlign: "center",
+    maxWidth: "9.375rem",
+    margin: "0 3rem 0 auto",
+    fontSize: "1.8rem",
+    color: "#fff",
+    background: "tomato",
+    lineHeight: 1,
+    padding: "0.5rem 1rem",
+    "&:before": {
+      display: "none",
     },
-    subtitle1: {
-        color: "tan",
+    [theme.breakpoints.up("md")]: {
+      textAlign: "center",
+      margin: "0 auto",
+      "&:nth-of-type(2n)": {
+        float: "none",
+        margin: "0 auto",
+      },
+      "&:nth-of-type(2n):before": {
+        display: "none",
+      },
     },
+  },
+  heading: {
+    color: "tomato",
+    padding: "3rem 0",
+    textTransform: "uppercase",
+  },
+  subHeading: {
+    color: "#fff",
+    padding: 0,
+    textTransform: "uppercase",
+  },
+  body1: {
+    color: "tomato",
+  },
+  subtitle1: {
+    color: "tan",
+  },
 }));
 
 const Resume = () => {
-    const classes = useStyles();
-    return (
-
-   
-
-
+  const classes = useStyles();
+  return (
     <Box component="header" className={classes.mainContainer}>
-
-<Typography variant="h4" align="center" className={classes.heading}>
-                Download Resume
+      <Link
+        onClick={Pdf}
+        variant="h4"
+        align="center"
+        className={classes.heading}>
+        Download Resume
+      </Link>
+      <Typography
+        variant="h4"
+        align="center"
+        className={classes.heading}>
+        Work Experience
       </Typography>
-
-      <Typography variant="h4" align="center" className={classes.heading}>
-                Work Experience
-      </Typography>
-      <Box component="div" className={classes.timeLine}>
+      <Box
+        component="div"
+        className={classes.timeLine}>
         <Typography
           variant="h2"
           className={`${classes.timeLineYear} ${classes.timeLineItem}`}
         >
-                    2020
+          2020
         </Typography>
         <Box component="div" className={classes.timeLineItem}>
           <Typography
@@ -138,26 +143,26 @@ const Resume = () => {
             align="center"
             className={classes.subHeading}
           >
-                        Automation Engineer
+            Automation Engineer
           </Typography>
           <Typography variant="body1" align="center" className={classes.body1}>
-                        RentPath
+            RentPath
           </Typography>
           <Typography
             variant="subtitle1"
             align="center"
             className={classes.subtitle1}
           >
-                        Maintaining and creating end-to-end tests forboth Web and Mobile applications in Nightwatch.js.
-                        Maintaining Jenkins pipeline and reviewingfailed tests.
-                        Created tests for new features.
+            Maintaining and creating end-to-end tests forboth Web and Mobile applications in Nightwatch.js.
+            Maintaining Jenkins pipeline and reviewingfailed tests.
+            Created tests for new features.
           </Typography>
         </Box>
         <Typography
           variant="h2"
           className={`${classes.timeLineYear} ${classes.timeLineItem}`}
         >
-                    2018 - 2020
+          2018 - 2020
         </Typography>
         <Box component="div" className={classes.timeLineItem}>
           <Typography
@@ -165,28 +170,28 @@ const Resume = () => {
             align="center"
             className={classes.subHeading}
           >
-                        Automation Engineer
+            Automation Engineer
           </Typography>
           <Typography variant="body1" align="center" className={classes.body1}>
-                        Hotel Effectiveness
+            Hotel Effectiveness
           </Typography>
           <Typography
             variant="subtitle1"
             align="center"
             className={classes.subtitle1}
           >
-                        Designed and implemented new features foriOS/Android applications.
-                        Creating and maintaining automation suitesusing JavaScriptwith Mocha and Chai.
-                        Successfully moved jobs from the Windowsserver to the Linux server.
-                        Created pipelines for projects with QA smokeand regression jobs to ensure quality throughoutthe software development life cycle.
-                        Developed software system testing anddocument design specifications, installationinstructions, and other system relatedinformation
+            Designed and implemented new features foriOS/Android applications.
+            Creating and maintaining automation suitesusing JavaScriptwith Mocha and Chai.
+            Successfully moved jobs from the Windowsserver to the Linux server.
+            Created pipelines for projects with QA smokeand regression jobs to ensure quality throughoutthe software development life cycle.
+            Developed software system testing anddocument design specifications, installationinstructions, and other system relatedinformation
           </Typography>
         </Box>
         <Typography
           variant="h2"
           className={`${classes.timeLineYear} ${classes.timeLineItem}`}
         >
-                    2018
+          2018
         </Typography>
         <Box component="div" className={classes.timeLineItem}>
           <Typography
@@ -194,24 +199,24 @@ const Resume = () => {
             align="center"
             className={classes.subHeading}
           >
-                        Front End iOS Intern
+            Front End iOS Intern
           </Typography>
           <Typography variant="body1" align="center" className={classes.body1}>
-                        Maze Services
+            Maze Services
           </Typography>
           <Typography
             variant="subtitle1"
             align="center"
             className={classes.subtitle1}
           >
-                        Experience in CSS, JavaScript – React.
-                        Worked with Android Studio and Simulator.
-                        Responsible for development of new highly-responsive, web-based user interface
+            Experience in CSS, JavaScript – React.
+            Worked with Android Studio and Simulator.
+            Responsible for development of new highly-responsive, web-based user interface
           </Typography>
         </Box>
       </Box>
     </Box>
-    );
+  );
 };
 
 export default Resume;
